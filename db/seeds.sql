39a3b8414b82f7db06affe2cb0c0fa011a196ac1
chasemillet <chase.millet@gmail.com>
2018-03-14 15:19:30 -0600

    awesome

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Glades Burger');
+INSERT INTO burgers(burger_name) VALUES ('Sea Burger');
+INSERT INTO burgers(burger_name) VALUES ('Western Burger');
+INSERT INTO burgers(burger_name) VALUES ('Awesome Burger');
+INSERT INTO burgers(burger_name) VALUES ('Butch Burger');
